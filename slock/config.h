a913f192128e65f28af7f5b0caef33b4606125b1
amrit-44404 <amrit44404@proton.me>
2024-08-02 14:21:41 +0545

    new color schemes

--- slock/config.h
+++ slock/config.h
@@ -4,10 +4,11 @@ static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
     [INIT] = "#0a0a0a",   /* after initialization */
-    [INPUT] = "#0ea5e9",  /* during input */
-    [FAILED] = "#f43f5e", /* wrong password */
-    [CAPS] = "#6366f1",   /* CapsLock on */
+    [INPUT] = "#005577",  /* during input */
+    [FAILED] = "#770000", /* wrong password */
+    [CAPS] = "#ebdbb2",   /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+

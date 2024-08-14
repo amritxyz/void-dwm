b4036995722e34ec70b9b6d4fb0142ae4839fd03
amrit-44404 <amrit44404@proton.me>
2024-08-14 22:48:08 +0545

    new slstatus bar

--- slstatus/config.h
+++ slstatus/config.h
@@ -1,5 +1,5 @@
 /* interval between updates (in ms) */
-const unsigned int interval = 1000;
+const unsigned int interval = 60000;
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
 /* maximum output string length */
@@ -11,12 +11,12 @@ static const struct arg args[] = {
 //	{ temp, 		"[TEMP %sC] ",	"/sys/class/thermal/thermal_zone0/temp" },
 //	{ ram_used,		"  %s",	NULL },
 	//{ datetime, 		" %s ",	"%a %b %d %r" },
-	{ cpu_perc, 	" 🗄️ CPU %s%%  ",NULL},
-	{ ram_perc,	"📟 RAM %s%%  ",NULL},
-	{ netspeed_rx,	"📡 %sB/s  ","wlp2s0" },
-	{ run_command,	"🔊 %1s  ","amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ datetime,	"📅 %s  ","%a, %B-%d"},
-	{ datetime,	"🕒 %s  ","%I:%M:%S %p"},
+//	{ cpu_perc, 	" 🗄️ CPU %s%%  ",NULL},
+//	{ ram_perc,	"📟 RAM %s%%  ",NULL},
+//	{ netspeed_rx,	"📡 %sB/s  ","wlp2s0" },
+//	{ run_command,	"🔊 %1s  ","amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ datetime,	"📅 %s  ","%a, %B %d"},
+	{ datetime,	"🕒 %s  ","%I:%M %p"},
 	{ battery_perc,	"☠ %s%%","BAT0"},
 	{ battery_state,"(%s)","BAT0" },
 };

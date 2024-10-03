8b4dfb1c85780ba7a8ca79b03d1ec5e53a3e2f7f
nyx-void <amrit44404@proton.me>
2024-10-03 23:43:54 +0545

    new minor changes

--- dwm/config.h
+++ dwm/config.h
@@ -205,8 +205,8 @@ static const Key keys[] = {
 	{ MODKEY,			XK_n,		spawn,		{.v = (const char*[]){ TERMINAL, "-e", "nvim", NULL } } },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat ") },
 	{ MODKEY,			XK_o,		spawn,		SHCMD("xdotool type $(grep -v '^#' ~/.local/share/snippets | dmenu -i -l 20 | cut -d' ' -f1)") },
-	{ MODKEY|ShiftMask,		XK_o,		spawn,		SHCMD("xdotool type $(grep -v '^#' ~/.local/share/hub | dmenu -i -l 20 | cut -d' ' -f1)") },
-	{ MODKEY|ShiftMask,		XK_i,		spawn,		SHCMD("xdotool type $(grep -v '^#' ~/.local/share/lab | dmenu -i -l 20 | cut -d' ' -f1)") },
+	{ MODKEY|ShiftMask,		XK_o,		spawn,		SHCMD("xdotool type $(grep -v '^#' ~/.local/share/lab | dmenu -i -l 20 | cut -d' ' -f1)") },
+	{ MODKEY|ShiftMask,		XK_i,		spawn,		SHCMD("xdotool type $(grep -v '^#' ~/.local/share/hub | dmenu -i -l 20 | cut -d' ' -f1)") },
 
 	{ MODKEY|ShiftMask,		XK_s,		spawn,		SHCMD("pkill -USR1 -x sxhkd") },
 	{ MODKEY|ShiftMask,		XK_f,		spawn,		SHCMD("~/.local/bin/scrot.sh")},

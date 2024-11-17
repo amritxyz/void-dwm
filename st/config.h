d7984bbb7ff9b03ef090d92a87241a0a3fff1b4f
nyx-void <amrit44404@proton.me>
2024-11-17 15:01:29 +0545

    new minor changes

--- st/config.h
+++ st/config.h
@@ -268,14 +268,14 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
 	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
-	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
+	/* { MODKEY,               XK_Up,          kscrollup,      {.i =  1} }, */
+	/* { MODKEY,               XK_Down,        kscrolldown,    {.i =  1} }, */
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
 	{ MODKEY,		XK_s,		changealpha,	{.f = -0.05} },
 	{ MODKEY,		XK_a,		changealpha,	{.f = +0.05} },
-	{ TERMMOD,              XK_Up,          zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
+	/* { TERMMOD,              XK_Up,          zoom,           {.f = +1} }, */
+	/* { TERMMOD,              XK_Down,        zoom,           {.f = -1} }, */
 	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
 	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_U,           zoom,           {.f = +2} },
@@ -287,7 +287,7 @@ static Shortcut shortcuts[] = {
 
 /*
  * Special keys (change & recompile st.info accordingly)
- *
+*
  * Mask value:
  * * Use XK_ANY_MOD to match the key no matter modifiers state
  * * Use XK_NO_MOD to match the key alone (no modifiers)

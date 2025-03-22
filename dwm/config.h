3f0782413238f0849d3adfb104fe6e5b27b8934f
amritxyz <amrit44404@proton.me>
2025-03-22 20:36:42 +0545

    who uses gimp chad use ImageMagick

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,6 @@
 #define TERMCLASS "St"
 #define BROWSER "firefox"
 #define BIT "bleachbit"
-#define GIMP "gimp"
 #define TORR "deluge-gtk"
 
 /* appearance */
@@ -162,7 +161,6 @@ static const Key keys[] = {
 	{ MODKEY,			XK_w,		spawn,		{.v = (const char*[]){ BROWSER, NULL } } },
 	{ MODKEY|ShiftMask,		XK_w,		spawn,		{.v = (const char*[]){ TERMINAL, "-e", "sudo", "nmtui", NULL } } },
 	{ MODKEY|ShiftMask,		XK_b,		spawn,		{.v = (const char*[]){ BIT, NULL } } },
-	{ MODKEY,			XK_y,		spawn,		{.v = (const char*[]){ GIMP, NULL } } },
 	{ MODKEY,			XK_e,		spawn,		{.v = (const char*[]){ TORR, NULL } } },
 	{ MODKEY,			XK_p,		spawn,		SHCMD(TERMINAL " -e alsamixer ") },
 	{ MODKEY,			XK_Escape,	spawn,		SHCMD(TERMINAL " -e htop") },
